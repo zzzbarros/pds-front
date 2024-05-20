f05f5dd7dc9a0bf1779b48a9e9d26302d686e9ce
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-05-19 23:57:32 -0300

    feat: (register) - integrate create password with api

--- services/api/index.ts
+++ services/api/index.ts
@@ -26,6 +26,49 @@ class API {
       },
     },
   }
+
+  public auth = {
+    createPassword: async (data: {
+      password: string
+      confirmPassword: string
+      token: string
+    }): Promise<{ ok: boolean; title: string; message: string; conflict?: boolean }> => {
+      try {
+        const res = await fetch(this.baseUrl.concat('auth/create-password'), {
+          method: 'POST',
+          headers: this.headers,
+          body: JSON.stringify(data),
+        })
+        const response = await res.json()
+        return { ok: res.ok, title: response.title, message: response.message }
+      } catch {
+        return {
+          ok: false,
+          title: 'Desculpe, parece que ocorreu um erro.',
+          message: 'Tente novamente em instantes...',
+        }
+      }
+    },
+    login: async (data: {
+      email: string
+      password: string
+    }): Promise<{ ok: boolean; title?: string; message?: string }> => {
+      try {
+        const res = await fetch(this.baseUrl.concat('auth/login'), {
+          method: 'POST',
+          headers: this.headers,
+          body: JSON.stringify(data),
+        })
+        return { ok: res.ok }
+      } catch {
+        return {
+          ok: false,
+          title: 'Desculpe, parece que ocorreu um erro.',
+          message: 'Tente novamente em instantes...',
+        }
+      }
+    },
+  }
 }
 
 export const services = new API()
