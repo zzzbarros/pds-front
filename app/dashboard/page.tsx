800351f0ff72485bdc1e252645702492e0999e54
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-02 22:39:52 -0300

    feat: (athlete): update dashboard home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import { getUser } from '../auth/get-user'
-
 export default function AuthPage() {
-  const { token } = getUser()
-
   return (
     <section className='p-4'>
       <div className='flex flex-col gap-0.5'>
         <h1 className='text-xl font-semibold'>Inicio</h1>
         <h2 className='text-lg'>Seja bem vindo a Plataforma!</h2>
-        <h3>Token de autenticação: {token}</h3>
+        <p>Utilize o menu ao lado para navegar entre as páginas...</p>
       </div>
     </section>
   )
