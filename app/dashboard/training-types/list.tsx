a344e6c38cd1e9803c3407b4d334f6fe919db0ce
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-02 22:39:33 -0300

    feat: (athlete): create and list training types

---
+++ app/dashboard/training-types/list.tsx
@@ -0,0 +1,70 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Pagination,
+  TableCaption,
+} from '@/components/ui'
+import { serverFetcher } from '@/services'
+import { PackageOpen } from 'lucide-react'
+
+interface TrainingTypesProps {
+  id: string
+  name: string
+  isEnabled: boolean
+}
+
+interface Props {
+  page: string
+  search: string
+}
+
+export default async function List({ page, search }: Props) {
+  const response = await serverFetcher(`training-types?page=${page}&search=${search}`, {
+    method: 'GET',
+    next: { tags: ['training-types'] },
+  })
+
+  const trainingTypes: TrainingTypesProps[] = response?.data?.data ?? []
+
+  return (
+    <>
+      <div className='rounded-md border mt-6'>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Tipo do Treino</TableHead>
+              <TableHead>Status</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {!!trainingTypes?.length &&
+              trainingTypes.map(({ id, name, isEnabled }) => (
+                <TableRow key={id}>
+                  <TableCell href={`/dashboard/training-types/${id}`}>{name}</TableCell>
+                  <TableCell href={`/dashboard/training-types/${id}`}>{isEnabled ? 'Ativo' : 'Inativo'}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+          {!trainingTypes?.length && (
+            <TableCaption className='py-2 pb-6 text-primary-medium font-medium'>
+              <PackageOpen size={64} strokeWidth={1} />
+              {search ? 'Não encontramos nenhum Tipo de Treino com esse filtro...' : 'A lista está vazia...'}
+              <br />
+              {!search && 'Experimente cadastrar um novo Tipo de Treino!'}
+            </TableCaption>
+          )}
+        </Table>
+      </div>
+      <Pagination
+        page={Number(page)}
+        lastPage={response?.data?.lastPage}
+        total={response?.data?.total}
+        className='mt-6 justify-end'
+      />
+    </>
+  )
+}
---
+++ app/dashboard/training-types/page.tsx
@@ -0,0 +1,53 @@
+import { Suspense } from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  Skeleton,
+} from '@/components/ui'
+import List from './list'
+import { Create } from './create'
+import { SearchInput } from '@/components/ui/search-input'
+
+interface Props {
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default async function AthleteList({ searchParams }: Props) {
+  const { page = '1', search = '' } = searchParams
+
+  return (
+    <section className='p-4 px-10'>
+      <div className='flex flex-col gap-0.5'>
+        <h1 className='text-xl font-semibold'>Lista de Tipos de Treino</h1>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='/dashboard'>Inicio</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Tipos de Treino</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className='pt-6 flex justify-between'>
+        <SearchInput placeholder='Procure pelo tipo de treino...' value={search as string} />
+        <Create />
+      </div>
+      <Suspense
+        fallback={
+          <div className='mt-6 w-full h-full flex justify-center items-center'>
+            <Skeleton className='w-full h-[200px] sm:h-[400px] rounded-md' />
+          </div>
+        }
+      >
+        <List page={page as string} search={search as string} />
+      </Suspense>
+    </section>
+  )
+}
