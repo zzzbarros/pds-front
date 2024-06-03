e1a404f50a9e69a44b5fc79a42766f49f86e1eb7
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-02 22:35:11 -0300

    feat: (services): create server and client fetcher's

---
+++ services/server-fetcher.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { getUser } from '@/app/auth/get-user'
+import { redirect } from 'next/navigation'
+
+const baseUrl = process.env.api ?? ''
+
+export async function serverFetcher<T = any>(url: string, props: RequestInit | undefined) {
+  const { token } = getUser()
+  const response = await fetch(baseUrl.concat(url), {
+    ...props,
+    headers: {
+      ...props?.headers,
+      ...(token && { Authorization: 'Bearer '.concat(token) }),
+      ...((props?.method === 'POST' || props?.method === 'PUT') && { 'Content-Type': 'application/json' }),
+    },
+  })
+
+  if (response.status === 401) return redirect('/auth/login')
+
+  return { ok: response.ok, data: (await response.json()) as T, status: response.status }
+}
