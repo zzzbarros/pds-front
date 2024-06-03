e7d40fc34edf5e3dd1fd1fd5dedfe1c8f8a8fc55
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-02 22:37:54 -0300

    feat: (user): create search input component

---
+++ components/ui/search-input.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useCallback, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { Input } from './input'
+import { Button } from './button'
+import { Search } from 'lucide-react'
+
+export function SearchInput({ value, placeholder }: { value: string; placeholder: string }) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const [search, setSearch] = useState(value)
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+
+      params.set('page', '1')
+
+      if (value) params.set(name, value)
+      else params.delete(name)
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
+  const handleSearch = useCallback(
+    (value: string) => router.push(pathname.concat('?').concat(createQueryString('search', value))),
+    [pathname, createQueryString]
+  )
+
+  return (
+    <div className='flex w-full'>
+      <Input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder={placeholder}
+        className='max-w-xs rounded-tr-none rounded-br-none focus-visible:ring-transparent'
+        onKeyDown={(e) => e.code === 'Enter' && handleSearch(search)}
+      />
+      <Button className='rounded-tl-none rounded-bl-none' onClick={() => handleSearch(search)}>
+        <Search size={20} />
+      </Button>
+    </div>
+  )
+}
