20bc816ae90647071ec884660fb501ebf8024007
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-07 10:22:44 -0300

    feat: (app): remove spinner to progress bar component

--- components/ui/router-progress-bar.tsx
+++ components/ui/router-progress-bar.tsx
@@ -1,5 +1,5 @@
 import NextTopLoader from 'nextjs-toploader'
 
 export function RouterProgressBar() {
-  return <NextTopLoader color='#8B6DD7' crawl={true} />
+  return <NextTopLoader color='#8B6DD7' showSpinner={false} />
 }
