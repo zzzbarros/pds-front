ec5acab7be6850c476f1e0d3e89b115592473f6b
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-06-23 23:12:26 -0300

    feat: (theme): create training card component

---
+++ components/features/training/card.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import type { ReactNode } from 'react'
+
+interface Props {
+  id: string
+  date: Date
+  trainingType: string | { id: string; name: string }
+  duration: number
+  pse: number
+  load: number
+  psr?: number
+  description?: string
+  isPlanned?: boolean
+  children?: ReactNode
+}
+
+export function BaseTrainingCard({ trainingType, description, duration, pse, psr, load, isPlanned, children }: Props) {
+  return (
+    <div
+      tabIndex={1}
+      className={cn(
+        'animate-[enter_0.8s] flex flex-col gap-0.5 bg-primary-medium w-full rounded-md p-2',
+        isPlanned && 'bg-primary-night'
+      )}
+    >
+      <p className='text-xs text-white text-ellipsis line-clamp-1'>
+        Tipo: <strong>{typeof trainingType === 'string' ? trainingType : trainingType.name}</strong>.
+      </p>
+      <p className='text-xs text-white text-ellipsis line-clamp-1'>
+        Duração: <strong>{duration} minutos</strong>.
+      </p>
+      <p className='text-xs text-white text-ellipsis line-clamp-1'>
+        PSE: <strong>{pse}</strong>.
+      </p>
+      <p className='text-xs text-white text-ellipsis line-clamp-1'>
+        Carga: <strong>{load} U.A.</strong>.
+      </p>
+      {psr && (
+        <p className='text-xs text-white text-ellipsis line-clamp-1'>
+          PSR: <strong>{psr}</strong>
+        </p>
+      )}
+      {description && (
+        <p className='text-xs text-white text-ellipsis line-clamp-2'>
+          Descrição: <strong>{description}</strong>.
+        </p>
+      )}
+      {children}
+    </div>
+  )
+}
