408e4a336657455b9f4d24681bdc4252f12c3b8c
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-09 23:02:56 -0300

    fix: (auth) - redirect authenticated user

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,8 @@
 import { redirect } from 'next/navigation'
+import { getUser } from './get-user'
 
 export default function AuthPage() {
+  const { user } = getUser()
+  if (user) redirect('/authenticated/')
   redirect('/auth/register')
 }
