db5fea27d85756755a1a7a48a0d09e5d3cfcb543
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-11 16:22:29 -0300

    feat: (forgot-password) - create and integrate forgot password

---
+++ app/auth/forgot-password/[id]/page.tsx
@@ -0,0 +1,158 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Info } from 'lucide-react'
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  useToast,
+} from '@/components/ui'
+import { validator } from '@/lib/validator'
+import { services } from '@/services'
+import { useRouter } from 'next-nprogress-bar'
+
+type FormProps = z.input<typeof schema>
+type OutputFormProps = z.output<typeof schema>
+
+const schema = z
+  .object({
+    password: z
+      .string()
+      .min(2, {
+        message: 'Senha é obrigatória',
+      })
+      .default('')
+      .refine(validator.password, 'Senha inválida'),
+    confirmPassword: z
+      .string()
+      .min(2, {
+        message: 'Confirmação de senha é obrigatória',
+      })
+      .default(''),
+    serverError: z.string().default('').optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.password !== data.confirmPassword) return false
+      return data
+    },
+    {
+      message: 'As senhas não coincidem.',
+      path: ['confirmPassword'],
+    }
+  )
+  .transform(({ password, confirmPassword }) => ({ password, confirmPassword }))
+
+export default function CreatePassword() {
+  const router = useRouter()
+  const { toast } = useToast()
+
+  const form = useForm<FormProps>({
+    resolver: zodResolver(schema),
+  })
+
+  const fields = [
+    {
+      name: 'password',
+      label: 'Senha',
+      info: ['Mínimo 8 caracteres', 'Letras', 'Números', 'Caracteres especiais'],
+    },
+    { name: 'confirmPassword', label: 'Confirmação de senha' },
+  ]
+
+  async function onSubmit(data: FormProps) {
+    const token = location.pathname.split('/').at(-1)
+    if (!token) return
+
+    const res = await services.auth.createPassword({ ...(data as OutputFormProps), token })
+
+    if (!res.ok) {
+      toast({
+        title: res.title,
+        description: res.message,
+        variant: 'destructive',
+      })
+      form.setError('serverError', {})
+    } else {
+      router.replace('/auth/login')
+      toast({
+        title: res.title,
+        description: res.message,
+        variant: 'success',
+      })
+    }
+  }
+
+  return (
+    <section className='w-full text-center'>
+      <h1 className='text-2xl text-primary-medium font-bold'>Cadastre a nova senha</h1>
+      <p className='mt-[6px] mb-6 text-zinc-600 font-medium'>Crie sua nova senha para acessar a plataforma.</p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col max-w-xs md:max-w-sm mx-auto gap-6 text-left'
+        >
+          <div className='flex flex-col gap-4 md:gap-3'>
+            {fields.map(({ label, name, info }, index) => (
+              <FormField
+                key={name}
+                control={form.control}
+                name={name as keyof FormProps}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      {info ? (
+                        <Popover>
+                          <PopoverTrigger className='flex items-center gap-1.5 pb-0.5'>
+                            {label}
+                            <Info size={16} />
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <p className='font-semibold'>Deve conter:</p>
+                            <ul className='list-disc px-4 py-1'>
+                              {info.map((data, index) => (
+                                <li key={data}>
+                                  {data}
+                                  {index !== info.length - 1 ? ';' : '.'}
+                                </li>
+                              ))}
+                            </ul>
+                          </PopoverContent>
+                        </Popover>
+                      ) : (
+                        label
+                      )}
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        {...(!index ? { autoFocus: true, tabIndex: 0 } : { autoComplete: 'current-password' })}
+                        type='password'
+                        placeholder='********'
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
+          </div>
+          <Button isLoading={form.formState.isSubmitting} type='submit'>
+            Salvar senha
+          </Button>
+        </form>
+      </Form>
+    </section>
+  )
+}
---
+++ app/auth/forgot-password/page.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import { useRouter } from 'next-nprogress-bar'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowLeft, Send } from 'lucide-react'
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  useToast,
+} from '@/components/ui'
+import { clientFetcher } from '@/services'
+import Link from 'next/link'
+
+type FormProps = z.input<typeof schema>
+
+const schema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: 'E-mail inválido' })
+      .min(1, {
+        message: 'E-mail é obrigatório',
+      })
+      .default(''),
+    serverError: z.string().default('').optional(),
+  })
+  .transform(({ email }) => ({ email }))
+
+export default function CreatePassword() {
+  const router = useRouter()
+  const { toast } = useToast()
+
+  const form = useForm<FormProps>({
+    resolver: zodResolver(schema),
+  })
+
+  async function onSubmit(data: FormProps) {
+    const token = location.pathname.split('/').at(-1)
+    if (!token) return
+
+    const res = await clientFetcher('auth/forgot-password', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+
+    if (!res.ok) {
+      toast({
+        title: res.data.title,
+        description: res.data.message,
+        variant: 'destructive',
+      })
+      form.setError('serverError', {})
+    } else {
+      router.replace('/auth/login')
+      toast({
+        title: res.data.title,
+        description: res.data.message,
+        variant: 'success',
+      })
+    }
+  }
+  const { isSubmitting } = form.formState
+
+  return (
+    <section className='w-full text-center'>
+      <h1 className='text-3xl md:text-2xl text-primary-medium font-bold'>Recuperar Senha</h1>
+      <div className='w-full flex justify-center'>
+        <p className='text-xl md:text-lg mt-[6px] mb-6 text-zinc-600 font-medium w-full md:w-2/4 text-balance'>
+          Caso tenha esquecido da senha, informe o seu e-mail abaixo para solicitar a recuperação.
+        </p>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col max-w-xs md:max-w-sm mx-auto gap-6 text-left'
+        >
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input {...field} autoFocus tabIndex={0} placeholder='Digite seu e-mail...' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isSubmitting} isLoading={isSubmitting} type='submit'>
+            {!isSubmitting && <Send size={20} />}
+            Enviar
+          </Button>
+          <Link href='/auth/login'>
+            <Button variant='ghost' type='button' className='w-fit text-primary-medium'>
+              <ArrowLeft size={20} />
+              Voltar
+            </Button>
+          </Link>
+        </form>
+      </Form>
+    </section>
+  )
+}
