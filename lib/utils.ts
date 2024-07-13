bf8d8b0c71751aeda39fcd4fbdf3e2cd1fb7957d
José Barros <61697278+zzzbarros@users.noreply.github.com>
2024-07-12 23:31:50 -0300

    feat: (ui) - create functions to generate random chart numbers

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,4 +10,16 @@ export function capitalizeFirstLetter(word: string): string {
   return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
+export function generateRandomNumber(min: number = 0, max: number = 1000): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export function generateRandomNumbersArray(count: number = 7, min: number = 0, max: number = 1000): number[] {
+  const randomNumbers: number[] = []
+  for (let i = 0; i < count; i++) {
+    randomNumbers.push(generateRandomNumber(min, max))
+  }
+  return randomNumbers
+}
+
 export * from './dates'
